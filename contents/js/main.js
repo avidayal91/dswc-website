e9195aa506975d15fe9b87b0cf07983fed983696
Avii Dayal <avidayal91@gmail.com>
2019-01-09 17:42:02 +0530

    initial homepage commit(building)

---
+++ contents/js/main.js
@@ -0,0 +1,37 @@
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        450:{
+            item:3
+        },
+        992:{
+            items:5
+        }
+    }
+})
