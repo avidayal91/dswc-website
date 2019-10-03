14d18279d048abddc716b074abba92b174c40ebd
Avi Dayal <avidayal91@gmail.com>
2019-10-03 18:44:45 +0530

    before submission

--- contents/js/main.js
+++ contents/js/main.js
@@ -1,4 +1,52 @@
-$('.owl-carousel').owlCarousel({
+//************Font size script*************//
+var root = document.querySelector(":root");
+var increaseFontButton = document.querySelector("#font-inc");
+var defaultFontButton = document.querySelector("#font-default");
+var decreaseFontButton = document.querySelector("#font-dec");
+
+increaseFontButton.addEventListener("click", function(){
+    root.style.fontSize= "20px";
+});
+
+defaultFontButton.addEventListener("click", function(){
+    root.style.fontSize= "16px";
+});
+
+decreaseFontButton.addEventListener("click", function(){
+    root.style.fontSize= "12px";
+});
+
+// ***************custom search box  script*********///
+var searchBtn = document.querySelector("#search-btn");
+var searchBox = document.querySelector("#custom-search");
+
+searchBtn.addEventListener("click", function(){
+    searchBox.classList.toggle("search-box-on");
+});
+
+//***** NAVIGATION SCRIPT ******//
+var menuButton = document.querySelector(".menu-toggler");
+var menuBar = document.querySelector("nav");
+
+var subMenuToggle = document.querySelectorAll(".drop-down");
+
+var screenSize = window.innerWidth;
+
+if (screenSize < "991") {
+    for(var i =0; i<subMenuToggle.length; i++) {
+      subMenuToggle[i].addEventListener("click", function(){
+         this.classList.toggle("drop-menu");
+});
+}
+}
+
+menuButton.addEventListener("click", function(){
+    menuBar.classList.toggle("menu-active");
+});
+
+
+//logo banner script //
+$('.logo-carousel').owlCarousel({
     loop:true,
     margin:10,
     autoplay:true,
@@ -21,29 +69,65 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-var menuButton = document.querySelector(".menu-toggler");
-var menuBar = document.querySelector("nav");
 
-var subMenuToggle = document.querySelectorAll(".drop-down");
+// Hero slide script //
+var currentBackground = 0;
+var backgrounds = [];
+backgrounds[0] = 'contents/images/home-carousel/nvm-02.jpg';
+backgrounds[1] = 'contents/images/home-carousel/nvm-01.jpg';
+backgrounds[2] = 'contents/images/home-carousel/nvm-03.jpg';
+backgrounds[3] = 'contents/images/home-carousel/nvm-04.jpg';
+backgrounds[4] = 'contents/images/home-carousel/nvm-05.jpg';
+backgrounds[5] = 'contents/images/home-carousel/nvm-06.jpg';
+backgrounds[6] = 'contents/images/home-carousel/nvm-07.jpg';
+backgrounds[7] = 'contents/images/home-carousel/nvm-08.jpg';
+//backgrounds[8] = 'contents/images/home-carousel/09.jpg';
+//backgrounds[9] = 'contents/images/home-carousel/10.jpg';
+//backgrounds[10] = 'contents/images/home-carousel/11.jpg';
+//backgrounds[11] = 'contents/images/home-carousel/12.jpg';
+//backgrounds[12] = 'contents/images/home-carousel/13.jpg';
+//backgrounds[13] = 'contents/images/home-carousel/14.jpg';
+//backgrounds[14] = 'contents/images/home-carousel/15.jpg';
+//backgrounds[15] = 'contents/images/home-carousel/16.jpg';
+//backgrounds[16] = 'contents/images/home-carousel/17.jpg';
+//backgrounds[17] = 'contents/images/home-carousel/18.jpg';
+//backgrounds[18] = 'contents/images/home-carousel/19.jpg';
+//backgrounds[19] = 'contents/images/home-carousel/20.jpg';
+//backgrounds[20] = 'contents/images/home-carousel/21.jpg';
 
-menuButton.addEventListener("click", function(){
-    menuBar.classList.toggle("menu-active");
+function changeBackground() {
+    currentBackground++;
+    if(currentBackground > 7) currentBackground = 0;
+
+    $('.hero-slide').fadeOut(100,function() {
+        $('.hero-slide').css({
+            'background-image' : "url('" + backgrounds[currentBackground] + "')"
+        });
+        $('.hero-slide').fadeIn(100);
+    });
+
+
+    setTimeout(changeBackground, 4000);
+}
+
+$(document).ready(function() {
+    setTimeout(changeBackground, 50);        
 });
 
-//var subMenu1 = document.querySelector("#sub-menu-1");
-//var dropDown1 = document.querySelector("#drop-menu-1");
-//
-//subMenu1.addEventListener("click", function(){
-//    dropDown1.classList.toggle("sub-menu-active");
+//*******SCROL BUTTON SCRIPT******//
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-//});
-//
-//subMenuToggle.addEventListener("click", function(){
-//   console.log("sub menu opened"); 
-//});
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
 
-for(var i =0; i<subMenuToggle.length; i++) {
-  subMenuToggle[i].addEventListener("click", function(){
-     this.classList.toggle("drop-menu");
-  });
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
---
+++ contents/js/map.js
@@ -0,0 +1,214 @@
+//********* INTERACTIVE MAP SCRIPT*****//
+var modalBtns = document.querySelectorAll('path');
+
+var modal = document.querySelectorAll('.modal');
+
+var closeBtn = document.querySelectorAll('.closeBtn');
+
+
+// When the user clicks the button, open the modal 
+modalBtns[3].onclick = function() {
+    modal[0].style.display = "block";
+}
+
+modalBtns[4].onclick = function() {
+    modal[1].style.display = "block";
+}
+
+modalBtns[5].onclick = function() {
+    modal[2].style.display = "block";
+}
+
+modalBtns[6].onclick = function() {
+    modal[3].style.display = "block";
+}
+
+modalBtns[7].onclick = function() {
+    modal[4].style.display = "block";
+}
+
+modalBtns[8].onclick = function() {
+    modal[5].style.display = "block";
+}
+
+modalBtns[9].onclick = function() {
+    modal[6].style.display = "block";
+}
+
+modalBtns[10].onclick = function() {
+    modal[5].style.display = "block";
+}
+
+modalBtns[11].onclick = function() {
+    modal[7].style.display = "block";
+}
+
+modalBtns[12].onclick = function() {
+    modal[8].style.display = "block";
+}
+
+modalBtns[13].onclick = function() {
+    modal[9].style.display = "block";
+}
+
+modalBtns[14].onclick = function() {
+    modal[10].style.display = "block";
+}
+
+modalBtns[15].onclick = function() {
+    modal[11].style.display = "block";
+}
+
+modalBtns[16].onclick = function() {
+    modal[12].style.display = "block";
+}
+
+modalBtns[17].onclick = function() {
+    modal[13].style.display = "block";
+}
+
+modalBtns[18].onclick = function() {
+    modal[14].style.display = "block";
+}
+
+modalBtns[19].onclick = function() {
+    modal[15].style.display = "block";
+}
+
+modalBtns[20].onclick = function() {
+    modal[16].style.display = "block";
+}
+
+modalBtns[21].onclick = function() {
+    modal[17].style.display = "block";
+}
+
+modalBtns[22].onclick = function() {
+    modal[18].style.display = "block";
+}
+
+modalBtns[23].onclick = function() {
+    modal[19].style.display = "block";
+}
+
+modalBtns[24].onclick = function() {
+    modal[20].style.display = "block";
+}
+
+modalBtns[25].onclick = function() {
+    modal[21].style.display = "block";
+}
+
+modalBtns[26].onclick = function() {
+    modal[22].style.display = "block";
+}
+
+modalBtns[27].onclick = function() {
+    modal[23].style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+closeBtn[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+
+closeBtn[1].onclick = function() {
+    modal[1].style.display = "none";
+}
+
+closeBtn[2].onclick = function() {
+    modal[2].style.display = "none";
+}
+
+closeBtn[3].onclick = function() {
+    modal[3].style.display = "none";
+}
+
+closeBtn[4].onclick = function() {
+    modal[4].style.display = "none";
+}
+
+closeBtn[5].onclick = function() {
+    modal[5].style.display = "none";
+}
+
+closeBtn[6].onclick = function() {
+    modal[6].style.display = "none";
+}
+
+closeBtn[7].onclick = function() {
+    modal[7].style.display = "none";
+}
+
+closeBtn[8].onclick = function() {
+    modal[8].style.display = "none";
+}
+
+closeBtn[9].onclick = function() {
+    modal[9].style.display = "none";
+}
+
+closeBtn[9].onclick = function() {
+    modal[9].style.display = "none";
+}
+
+closeBtn[9].onclick = function() {
+    modal[9].style.display = "none";
+}
+
+closeBtn[10].onclick = function() {
+    modal[10].style.display = "none";
+}
+
+closeBtn[11].onclick = function() {
+    modal[11].style.display = "none";
+}
+
+closeBtn[12].onclick = function() {
+    modal[12].style.display = "none";
+}
+
+closeBtn[13].onclick = function() {
+    modal[13].style.display = "none";
+}
+
+closeBtn[14].onclick = function() {
+    modal[14].style.display = "none";
+}
+
+closeBtn[15].onclick = function() {
+    modal[15].style.display = "none";
+}
+
+closeBtn[16].onclick = function() {
+    modal[16].style.display = "none";
+}
+
+closeBtn[17].onclick = function() {
+    modal[17].style.display = "none";
+}
+
+closeBtn[18].onclick = function() {
+    modal[18].style.display = "none";
+}
+
+closeBtn[19].onclick = function() {
+    modal[19].style.display = "none";
+}
+
+closeBtn[20].onclick = function() {
+    modal[20].style.display = "none";
+}
+
+closeBtn[21].onclick = function() {
+    modal[21].style.display = "none";
+}
+
+closeBtn[22].onclick = function() {
+    modal[22].style.display = "none";
+}
+
+closeBtn[23].onclick = function() {
+    modal[23].style.display = "none";
+}
